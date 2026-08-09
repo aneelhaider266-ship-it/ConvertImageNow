4a8a3e81732475e1011e25a36f223a22e5fc1b21
aneelhaider266-ship-it <aneelhaider266@gmail.com>
2026-08-09 07:25:19 +0500

    Update sitemap URL to use www subdomain

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://convertimagenow.com/sitemap.xml",
+    sitemap: "https://www.convertimagenow.com/sitemap.xml",
   };
 }
