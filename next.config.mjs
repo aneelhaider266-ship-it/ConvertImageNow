65eb2ba4c5e84b4592b05135de7396e59d344305
aneelhaider266-ship-it <aneelhaider266@gmail.com>
2026-08-09 07:44:21 +0500

    Add redirects for convertimagenow.com

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'convertimagenow.com',
+          },
+        ],
+        destination: 'https://www.convertimagenow.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
