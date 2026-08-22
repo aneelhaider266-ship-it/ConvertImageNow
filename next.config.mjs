5a476c1d6d3070a6a72a57604bc0b0bf76c67420
aneelhaider266-ship-it <aneelhaider266@gmail.com>
2026-08-22 12:31:42 +0500

    Fix quotes and update redirect paths in config

--- next.config.mjs
+++ next.config.mjs
@@ -3,24 +3,30 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
+
   async redirects() {
     return [
       {
-        source: '/:path*',
+        source: "/:path*",
         has: [
           {
-            type: 'host',
-            value: 'convertimagenow.com',
+            type: "host",
+            value: "convertimagenow.com",
           },
         ],
-        destination: 'https://www.convertimagenow.com/:path*',
+        destination: "https://www.convertimagenow.com/:path*",
         permanent: true,
       },
       {
-          source: '/blog/heic-to-jpg-guide',
-          destination: '/blog/how-to-convert-heic-to-jpg',
-          permanent: true,
-        },
+        source: "/blog/how-to-reduce-image-size",
+        destination: "/blog/how-to-make-image-file-smaller",
+        permanent: true,
+      },
+      {
+        source: "/blog/heic-to-jpg-guide",
+        destination: "/blog/how-to-convert-heic-to-jpg",
+        permanent: true,
+      },
     ];
   },
 };
