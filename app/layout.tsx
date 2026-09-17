f538a4de2160033ce2154fc94b39c70457f896a3
aneelhaider266-ship-it <aneelhaider266@gmail.com>
2026-09-17 18:28:41 +0500

    Update Google Analytics ID and loading strategy

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,16 +100,17 @@ export default function RootLayout({
 
         {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-HNPJHT8NMN"
-          strategy="lazyOnload"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-HZ0NXP679X"
+          strategy="afterInteractive"
         />
 
-        <Script id="google-analytics" strategy="lazyOnload">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-HNPJHT8NMN');
+            gtag('config', 'G-HZ0NXP679X');
           `}
         </Script>
       </head>
